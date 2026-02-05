4946d22e1a7a9cc06823a65f310c9c69372dcf8d
Abhishek_R_P <theabhishekrp@gmail.com>
2026-02-05 14:40:51 +0530

    v2

--- cpulse-frontend/src/api/axios.ts
+++ cpulse-frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://cpulse-backend.vercel.app',
+  baseURL: process.env.REACT_APP_API_URL || 'https://cpulse-backend.vercel.app',
 });
 
 api.interceptors.request.use((config) => {
