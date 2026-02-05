904347ee3ece59264b0ea4c173e8bd261b012c4b
Abhishek_R_P <theabhishekrp@gmail.com>
2026-02-05 14:40:51 +0530

    v2

--- cpulse-frontend/src/components/ProblemOfTheDay.tsx
+++ cpulse-frontend/src/components/ProblemOfTheDay.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
+import api from "../api/axios";
 
 interface Problem {
   id: string;
@@ -127,7 +127,7 @@ export default function ProblemOfTheDay() {
     const fetchProblemOfDay = async () => {
       try {
         setLoading(true);
-        const res = await axios.get("/api/daily-problem");
+        const res = await api.get("/api/daily-problem");
         setProblem(res.data);
         setLoading(false);
       } catch (err) {
